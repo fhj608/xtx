f2acd5caf91fc9676248074e4a5d50e444afc623
fhj <fhj608@163.com>
2023-08-20 10:12:43 +0800

    element主题色定制

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+</script>
+
 <template>
-  <el-button type="primary">button</el-button>
+  <el-config-provider :locale="zhCn">
+    <el-button type="primary">button</el-button>
+  </el-config-provider>
 </template>
